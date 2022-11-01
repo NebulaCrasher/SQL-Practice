8162e220753d4630b2d5017d22d018f7ada6b245
NebulaCrasher <dksor96@gmail.com>
2022-11-01 16:44:21 -0500

    Updating README file and adjusting some comments

--- Employee SQL/SQL_queries.sql
+++ Employee SQL/SQL_queries.sql
@@ -1,45 +1,45 @@
---Listing employee info and their salaries
+-- 1. Listing employee info and their salaries
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
 
---Listing all employees hired in 1986
+-- 2. Listing all employees hired in 1986
 SELECT last_name, first_name, hire_date
 FROM employees
 WHERE hire_date >= '1986-01-01' AND hire_date <= '1986-12-31';
 
---Listing all department managers along with their first and last names.
+-- 3. Listing all department managers along with their first and last names.
 SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
 FROM departments
 INNER JOIN dept_manager ON  departments.dept_no = dept_manager.dept_no
 INNER JOIN employees ON employees.emp_no = dept_manager.emp_no;
 
---Listing employees first and last names along with their department name
+-- 4. Listing employees first and last names along with their department name
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM employees
 INNER JOIN dept_emp ON  employees.emp_no = dept_emp.emp_no
 INNER JOIN departments ON dept_emp.dept_no = departments.dept_no;
 
---Listing every employee with first name Hercules and last name starting with B
+-- 5. Listing every employee with first name Hercules and last name starting with B
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
---Listing all Sales department employees
+-- 6. Listing all Sales department employees
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM employees
 INNER JOIN dept_emp ON  employees.emp_no = dept_emp.emp_no
 INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE dept_emp.dept_no = 'd007';
 
---Listing all employees part of the sales and development departments
+-- 7. Listing all employees part of the sales and development departments
 SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM employees
 INNER JOIN dept_emp ON  employees.emp_no = dept_emp.emp_no
 INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
 WHERE dept_emp.dept_no = 'd007' OR dept_emp.dept_no = 'd005';
 
--- Counting last name frequency 
+-- 8. Counting last name frequency 
 SELECT last_name, COUNT(last_name)
 FROM employees
 GROUP BY last_name
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sql-challenge
 Learning how to utilize SQL in order to analyze past Employee records
+
+## Creating SQL Tables
+The first SQL file provided will set up the schema of the tables necessary for the assignment. The column headers of the csv files pretty much match 
+1:1 with the SQL table data heads. Run this first and import the data to the created tables.
+
+## SQL Queries
+This file provides all relevant queries to answer the questions posed by the new employer. 
